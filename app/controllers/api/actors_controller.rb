8e038c7dc5135a49f0f43f78b1917604c087fb5b
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-02-25 11:58:09 -0500

    Working on adding the id

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,5 +1,8 @@
 class Api::ActorsController < ApplicationController
   def actor1_action
+    @id = 1
+    @actor_id = Actor.find_by(id: @id)
+
     @actor_one = Actor.first
     render "actors.json.jb"
   end
