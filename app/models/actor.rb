de2e492c5317035414340058dc60627f7779c297
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-09 18:04:54 -0500

    Makes associations between actors and movies. HW Week4Day1

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,7 @@
 class Actor < ApplicationRecord
+  belongs_to :movie #returns hash of one single movie
   validates :first_name, length: { minimum: 2 }
   validates :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
-  validates :age, length: { minimum: 13 }
+  validates :age, numericality: { greater_than: 13 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors #returns array of many actors
   validates :title, length: { minimum: 2 }
   validates :year, presence: true
   validates :plot, length: { minimum: 10 }
