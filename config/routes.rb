081a63689538e85dc9df5811f13b9b8cb650abb1
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-14 11:43:58 -0400

    Authenticate for admin in movie-app

--- config/routes.rb
+++ config/routes.rb
@@ -23,17 +23,19 @@ Rails.application.routes.draw do
     ######MOVIES#######
     #GET "movies" - Gets all the movies
     get "/movies" => "movies#index"
-
     # GET "/movies/3" - Just get one specific movie (that already exists)
     get "/movies/:id" => "movies#show"
-
     # POST "/movies" - Creates a new movie, add it to our database
     post "/movies" => "movies#create"
-
     # PATCH "/movies/3" - Updates a specific movie
     patch "/movies/:id" => "movies#update"
-
     # DELETE "/movies/3" - Deletes a specific movie
     delete "/movies/:id" => "movies#destroy"
+
+    ###### USERS #######
+    post "/users" => "users#create"
+
+    ###### SESSIONS #######
+    post "/sessions" => "sessions#create"
   end
 end
