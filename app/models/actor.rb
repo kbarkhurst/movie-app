0d1e55a3fb09b6f6f5665b261e4c86db8d9e7e35
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-14 13:52:24 -0400

    Adds many-to-many with genres

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,6 @@
 class Actor < ApplicationRecord
   belongs_to :movie #returns hash of one single movie
+  
   validates :first_name, length: { minimum: 2 }
   validates :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 class Movie < ApplicationRecord
   has_many :actors #returns array of many actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   validates :title, length: { minimum: 2 }
   validates :year, presence: true
   validates :plot, length: { minimum: 10 }
